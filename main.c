1bc00e344c49d8f0311416ce3dc36defc2416482
btr13010 <tranquocbao290402@gmail.com>
2023-04-23 21:42:30 +0700

    Update opcode cases

--- main.c
+++ main.c
@@ -6,9 +6,10 @@
 uint16_t memory[MEMORY_MAX];
 
 /*
-    Create an enum to store registers. 
+    Create an enum to store registers.
     The registers are used to store data and addresses during the execution of the program. 
-    The CPU uses the registers to perform operations on the data and addresses.
+    The CPU uses these data and addresses to perform operations.
+    8 registers are used to store data: R0-R7, and 2 registers are used to store addresses: PC and COND.
 */
 enum
 {
@@ -26,7 +27,15 @@ enum
 };
 uint16_t reg[R_COUNT];
 
-// Create an enum to store opcodes
+// Create an enum to store the set of 3 condition flags which indicate the sign of the previous calculation
+enum
+{
+    FL_POS = 1 << 0, /* P */
+    FL_ZRO = 1 << 1, /* Z */
+    FL_NEG = 1 << 2, /* N */
+};
+
+// Create an enum to store the set of operations that the CPU can perform (opcodes)
 enum
 {
     OP_BR = 0, /* branch */
@@ -46,3 +55,99 @@ enum
     OP_LEA, /* load effective address */
     OP_TRAP /* execute trap */
 };
+
+int main(int argc, const char* argv[]) {
+    /*
+        The CPU has 3 main phases:
+        1. Fetch: fetch the instruction from memory at the address of the PC register and increment the PC register
+        2. Decode: decode the instruction by looking at the opcode to determine the operation to be performed
+        3. Execute: execute the operation using the parameters in the instruction
+    */
+
+    // Handle command line inputs
+    if (argc < 2)
+    {
+        /* show usage string */
+        printf("lc3 [image-file1] ...\n");
+        exit(2);
+    }
+    
+    for (int j = 1; j < argc; ++j)
+    {
+        if (!read_image(argv[j]))
+        {
+            printf("failed to load image: %s\n", argv[j]);
+            exit(1);
+        }
+    }
+
+    @{Setup}
+
+    /* since exactly one condition flag should be set at any given time, set the Z flag */
+    reg[R_COND] = FL_ZRO;
+
+    /* set the PC to starting position */
+    /* 0x3000 is the default */
+    enum { PC_START = 0x3000 };
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+    while (running)
+    {
+        /* FETCH */
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+
+        switch (op)
+        {
+            case OP_ADD:
+                @{ADD}
+                break;
+            case OP_AND:
+                @{AND}
+                break;
+            case OP_NOT:
+                @{NOT}
+                break;
+            case OP_BR:
+                @{BR}
+                break;
+            case OP_JMP:
+                @{JMP}
+                break;
+            case OP_JSR:
+                @{JSR}
+                break;
+            case OP_LD:
+                @{LD}
+                break;
+            case OP_LDI:
+                @{LDI}
+                break;
+            case OP_LDR:
+                @{LDR}
+                break;
+            case OP_LEA:
+                @{LEA}
+                break;
+            case OP_ST:
+                @{ST}
+                break;
+            case OP_STI:
+                @{STI}
+                break;
+            case OP_STR:
+                @{STR}
+                break;
+            case OP_TRAP:
+                @{TRAP}
+                break;
+            case OP_RES:
+            case OP_RTI:
+            default:
+                @{BAD OPCODE}
+                break;
+        }
+    }
+    @{Shutdown}
+}

c38c7d572adfebf5a3e9d58fa668a159ba64271c
btr13010 <tranquocbao290402@gmail.com>
2023-04-23 15:52:40 +0700

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # virtual-machine
+
+A simple LC-3 virtual machine written in C 
+
+**Contributors**: Tran Quoc Bao, Tran Huy Hoang Anh, Pham Anh Quan
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdint.h>
+
+// Create an array to store memory locations
+#define MEMORY_MAX (1 << 16) 
+uint16_t memory[MEMORY_MAX];

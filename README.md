9561982ec7db041e5244214ebdc98fb7e66c74b4
btr13010 <tranquocbao290402@gmail.com>
2023-04-23 17:34:48 +0700

    Add registers

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # virtual-machine
 
-A simple LC-3 virtual machine written in C 
+Midterm Project ELEC2030
+
+**Description**: A simple LC-3 virtual machine written in C
 
 **Contributors**: Tran Quoc Bao, Tran Huy Hoang Anh, Pham Anh Quan
--- main.c
+++ main.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
 #include <stdint.h>
 
-// Create an array to store memory locations
-#define MEMORY_MAX (1 << 16) 
+// Create an array to store memory addresses
+#define MEMORY_MAX (1 << 16) // use 16 bits to store the memory address => MEMORY_MAX = 2^16 = 65536
 uint16_t memory[MEMORY_MAX];
+
+/*
+    Create an enum to store registers. 
+    The registers are used to store data and addresses during the execution of the program. 
+    The CPU uses the registers to perform operations on the data and addresses.
+*/
+enum
+{
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC, // program counter: memory address of the next instruction to be executed
+    R_COND, // condition flag: information about the previous operation
+    R_COUNT
+};
+uint16_t reg[R_COUNT];
+
+// Create an enum to store opcodes
+enum
+{
+    OP_BR = 0, /* branch */
+    OP_ADD, /* add  */
+    OP_LD, /* load */
+    OP_ST, /* store */
+    OP_JSR, /* jump register */
+    OP_AND, /* bitwise and */
+    OP_LDR, /* load register */
+    OP_STR, /* store register */
+    OP_RTI, /* unused */
+    OP_NOT, /* bitwise not */
+    OP_LDI, /* load indirect */
+    OP_STI, /* store indirect */
+    OP_JMP, /* jump */
+    OP_RES, /* reserved (unused) */
+    OP_LEA, /* load effective address */
+    OP_TRAP /* execute trap */
+};

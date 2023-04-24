3b77a981c33bf55704c7e78e0b99c32fa3b9330e
btr13010 <tranquocbao290402@gmail.com>
2023-04-24 13:42:08 +0700

    Update memory mapped registers

--- main.c
+++ main.c
@@ -9,11 +9,60 @@
     to for the CPU to execute. The opcodes and their instructions are also provided 
     for the CPU to execute the commands.
 
-    The program is written in C language.
+    The program is written in the C language.
 */
 
 #include <stdio.h>
 #include <stdint.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
+
+// Input buffer size
+struct termios original_tio;
+
+void disable_input_buffering() {
+    /*
+        This function disables the input buffering.
+        Input buffering is a mechanism that allows the operating system to hold the input until the user presses the enter key.
+        This function is used to disable the input buffering so that the program can read the input immediately.
+    */
+
+    tcgetattr(STDIN_FILENO, &original_tio);
+    struct termios new_tio = original_tio;
+    new_tio.c_lflag &= ~ICANON & ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+void restore_input_buffering() {
+    /*
+        This function restores the input buffering.
+        This function is used to restore the input buffering after the program has finished reading the input.
+    */
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
+
+uint16_t check_key() {
+    /*
+        This function checks if the user has pressed a key.
+        If the user has pressed a key, the function returns 1, otherwise it returns 0.
+    */
+
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+    return select(1, &readfds, NULL, NULL, &timeout) != 0;
+}
 
 // Create an array to store memory addresses
 #define MEMORY_MAX (1 << 16) // using bitwise operation to define macro MEMORY_MAX = 2^16 = 65536
@@ -122,6 +171,49 @@ int read_image(const char* image_path) {
     return 1;
 }
 
+// Create an enum to store the memory mapped registers which are not accessible from the normal register table
+// Memory mapped registers are used to communicate with the special hardware devices
+enum
+{
+    MR_KBSR = 0xFE00, // keyboard status register indicates if a key has been pressed
+    MR_KBDR = 0xFE02  // keyboard data register indicates which key has been pressed
+};
+
+void mem_write(uint16_t address, uint16_t val) {
+    /*
+        This function writes a value to a memory address.
+    */
+
+    memory[address] = val;
+}
+
+uint16_t mem_read(uint16_t address) {
+    /*
+        This function reads a value from a memory address.
+    */
+
+    if (address == MR_KBSR) {
+        if (check_key()) {
+            memory[MR_KBSR] = (1 << 15);
+            memory[MR_KBDR] = getchar();
+        } else {
+            memory[MR_KBSR] = 0;
+        }
+    }
+    return memory[address];
+}
+
+void handle_interrupt(int signal) {
+    /*
+        This function handles the interrupt signal.
+        When the user presses Ctrl+C, the program will terminate. 
+    */
+
+    restore_input_buffering();
+    printf("\n");
+    exit(-2);
+}
+
 int main(int argc, const char* argv[]) {
     /*
         The CPU has 3 main phases:
@@ -131,23 +223,22 @@ int main(int argc, const char* argv[]) {
     */
 
     // Handle command line inputs
-    if (argc < 2)
-    {
+    if (argc < 2) {
         /* show usage string */
         printf("lc3 [image-file1] ...\n");
         exit(2);
     }
     
-    for (int j = 1; j < argc; ++j)
-    {
-        if (!read_image(argv[j]))
-        {
+    for (int j = 1; j < argc; ++j) {
+        if (!read_image(argv[j])) {
             printf("failed to load image: %s\n", argv[j]);
             exit(1);
         }
     }
 
-    @{Setup}
+    // Setup
+    signal(SIGINT, handle_interrupt);
+    disable_input_buffering();
 
     /* since exactly one condition flag should be set at any given time, set the Z flag */
     reg[R_COND] = FL_ZRO;
@@ -215,5 +306,7 @@ int main(int argc, const char* argv[]) {
                 break;
         }
     }
-    @{Shutdown}
+
+    // When the program is interrupted, the terminal settings is restored back to normal.
+    restore_input_buffering();
 }
